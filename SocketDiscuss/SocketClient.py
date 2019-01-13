bd47db1e598ac0d948617bc181933b1f9b453a0b
Shadix Aced <30985701+ShadixAced@users.noreply.github.com>
2019-01-13 13:19:53 +0100

    Update SocketClient.py
    ask user for ip

--- SocketDiscuss/SocketClient.py
+++ SocketDiscuss/SocketClient.py
@@ -2,6 +2,8 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print "Socket OK"
 port = 6969
-s.connect(('//insert distant IP', port))
+print = "Adresse IP de la personne que vous souhaitez joindre"
+i = input()
+s.connect((i, port))
 print s.recv(1024)
 s.close()
