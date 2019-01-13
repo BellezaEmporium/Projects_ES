648e2bbd5f7a11a0ddfc21835b598c7d8c5afdf7
Shadix Aced <ismael.elkihel@gmail.com>
2019-01-13 17:03:02 +0100

    reworking some parts (thanks again to Kao)

--- SocketDiscuss/SocketClient.py
+++ SocketDiscuss/SocketClient.py
@@ -1,19 +1,19 @@
-# -*- coding: cp1252 -*-
+# -*- coding: utf-8 -*-
 import socket
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print "Socket OK"
-print "Quel est le port utilisé par le serveur"
-p = input()
-print "Adresse IP de la personne que vous souhaitez joindre"
-i = input()
+p = input("Port du serveur :\n")
+i = raw_input("Adresse IP du serveur :\n")
 s.connect((i, p))
 msgServeur = s.recv(1024)
 while 1:
-       print "Administrateur>", msgServeur
-       if msgServeur.upper() == "FIN" or msgServeur =="":
+    print "Administrateur>", msgServeur
+    if msgServeur.upper() == "FIN" or msgServeur == "" or msgClient.upper() == "FIN" or msgClient == "":
+        s.close()
         break
-       msgClient = raw_input("Vous> ")
-       s.send(msgClient)
-       msgServeur = s.recv(1024)
+    msgClient = raw_input("Vous> ")
+    s.send(msgClient)
+    msgServeur = s.recv(1024)
 
 s.close()
--- SocketDiscuss/SocketServer.py
+++ SocketDiscuss/SocketServer.py
@@ -1,14 +1,21 @@
-import socket                 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)          
+# -*- coding: utf-8 -*-
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print "Socket OK"
 print "Quel port souhaitez vous utiliser"
 p = input()
-s.bind(('', p))      
-print "Le socket est en lien avec ce port-ci %s" %(p) 
-s.listen(5)      
-print "Le socket regarde la connexion..."            
-while True: 
-   c, addr = s.accept()      
-   print 'Sire je pense avoir vu une personne a cette adresse', addr 
-   c.send('Vous faites partie des notres sur ce serveur') 
-   c.close() 
+s.bind(('', p))
+print "Le socket est en lien avec ce port-ci %s" % (p)
+s.listen(5)
+print "Le socket regarde la connexion..."
+c, addr = s.accept()
+while True:
+    print 'Sire je pense avoir vu une personne a cette adresse', addr
+    c.send('Vous faites partie des notres sur ce serveur')
+    msgServeur = raw_input("Vous> ")
+    c.send(msgServeur)
+    msgClient = c.recv(1024)
+    print msgClient
+    if msgClient.upper() == "FIN" or msgClient == "":
+    c.close()
+    break
