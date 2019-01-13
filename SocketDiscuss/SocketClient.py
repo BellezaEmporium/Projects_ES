0be09e26fa9a69b14e866243e4d70b632aa28712
Shadix Aced <30985701+ShadixAced@users.noreply.github.com>
2019-01-13 13:15:46 +0100

    Update SocketClient.py

--- SocketDiscuss/SocketClient.py
+++ SocketDiscuss/SocketClient.py
@@ -2,6 +2,6 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print "Socket OK"
 port = 6969
-s.connect(('192.168.175.128', port))
+s.connect(('//insert distant IP', port))
 print s.recv(1024)
 s.close()
