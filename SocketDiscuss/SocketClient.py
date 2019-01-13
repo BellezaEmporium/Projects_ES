23207f1c0c50c8d8c700dab6526f5ea6c8550ba7
Shadix Aced <ismael.elkihel@gmail.com>
2019-01-13 15:02:55 +0100

    You can now discuss.

--- SocketDiscuss/SocketClient.py
+++ SocketDiscuss/SocketClient.py
@@ -1,7 +1,15 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print "Socket OK"
-port = 6969
+port = 62541
 s.connect(('192.168.175.128', port))
-print s.recv(1024)
+msgServeur = s.recv(1024)
+while 1:
+       print "Administrateur>", msgServeur
+       if msgServeur.upper() == "FIN" or msgServeur =="":
+        break
+       msgClient = raw_input("Vous> ")
+       s.send(msgClient)
+       msgServeur = s.recv(1024)
+
 s.close()
